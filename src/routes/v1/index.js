5a75f15cdd73665433d668805e5af032dc8bd640
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-07-17 23:16:15 +0530

    feat: Added v1 routes for route handling

---
+++ src/routes/v1/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+const { createStorageSpace,renameStorageSpace,deleteStorageSpace,getItemsInStorageSpace } = require("../../controllers/storage-space-controller.js");
+const { createItemType,renameItemType,deleteItemType } = require("../../controllers/item-types-controller.js");
+const { createItem, getAllItems, relocateItem, deleteItem } = require("../../controllers/items-controller.js");
+
+// Storage Spaces API
+router.post('/storage-spaces', createStorageSpace);
+router.put('/storage-spaces/:id', renameStorageSpace);
+router.delete('/storage-spaces/:id', deleteStorageSpace);
+router.get('/storage-spaces/:id/items', getItemsInStorageSpace);
+
+// Item Types API
+router.post('/item-types', createItemType);
+router.put('/item-types/:id', renameItemType);
+router.delete('/item-types/:id', deleteItemType);
+
+// Items API
+router.post('/items', createItem);
+router.get('/items', getAllItems);
+router.put('/items/:id/move', relocateItem);
+router.delete('/items/:id', deleteItem);
+
+module.exports = router;
