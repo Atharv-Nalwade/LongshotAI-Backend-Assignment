3f8595e25c08f0682898ae377226b8675dca6b32
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-07-19 18:33:46 +0530

    fix: Fixed mongo uri's for npm start and docker compose

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,21 @@
 const mongoose = require("mongoose");
 
- const connect = async () =>{
-    // await mongoose.connect('mongodb://localhost/GroceryApp');
-    await mongoose.connect('mongodb://mongo:27017/GroceryApp');
-    // await mongoose.connect('mongodb://192.168.1.100:27017/GroceryApp');
+const connect = async () => {
+  const mongoURI = process.env.MONGODB_URL || "mongodb://localhost/GroceryApp";
 
-    console.log("Connected to database");
-}
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to the database");
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+  }
+};
 
 module.exports = connect;
+
+// await mongoose.connect('mongodb://localhost/GroceryApp');
+// console.log(process.env.MONGODB_URL);
+// await mongoose.connect('mongodb://mongo:27017/GroceryApp') || await mongoose.connect('mongodb://localhost/GroceryApp');
+//  await mongoose.connect('mongodb://192.168.1.100:27017/GroceryApp');
+
+// console.log("Connected to database");
